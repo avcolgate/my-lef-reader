4b6911d36b10bb8b47a7e4bfa6208acc31fb453b
avcolgate <avcolgate@gmail.com>
2022-12-09 17:09:28 +0300

    simplified for size calculation

--- classes.py
+++ classes.py
@@ -1,14 +1,4 @@
 class Macro:
     def __init__(self, name=''):
         self.name = name
-        self.class_type = 'None'
         self.size = {0, 0}
-        self.symmetry = 'None'
-        self.pins = []
-
-
-class Pin:
-    def __init__(self, name=''):
-        self.name = name
-        self.direction = 'None'
-        self.layers = []
